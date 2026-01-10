edcf40a27f70bc1b43b96c8367d37d6347ad9df2
Kevin Amaguaña <71292836+Kevin13201j@users.noreply.github.com>
2026-01-10 00:39:53 -0500

    feat: implement User entity and register in db module

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -2,19 +2,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user.entity'; // <--- IMPORTANTE: Importar la entidad
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5433,            // OJO: Es el puerto que abrimos en Docker
-      username: 'admin',     // Usuario del docker-compose
-      password: 'adminpassword', // Contraseña del docker-compose
-      database: 'dental_db', // Base de datos del docker-compose
+      port: 5433,
+      username: 'admin',
+      password: 'adminpassword',
+      database: 'dental_db',
       autoLoadEntities: true,
-      synchronize: true,     // Solo para desarrollo (crea tablas automático)
+      synchronize: true, 
     }),
+    TypeOrmModule.forFeature([User]), // <--- IMPORTANTE: Registrar la tabla aquí
   ],
   controllers: [AppController],
   providers: [AppService],
