5e98b1d7972b6582f7ab4bb85f1128bb4ee256af
Kevin Amaguaña <71292836+Kevin13201j@users.noreply.github.com>
2026-01-10 00:16:57 -0500

    fix: resolve nestjs dependencies and connect auth-service to postgres

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,            // OJO: Es el puerto que abrimos en Docker
+      username: 'admin',     // Usuario del docker-compose
+      password: 'adminpassword', // Contraseña del docker-compose
+      database: 'dental_db', // Base de datos del docker-compose
+      autoLoadEntities: true,
+      synchronize: true,     // Solo para desarrollo (crea tablas automático)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
