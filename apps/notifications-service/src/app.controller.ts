c2d54842428e1ba0a901c09fa3fcabbddcc8805b
Kevin Amaguaña <71292836+Kevin13201j@users.noreply.github.com>
2026-01-11 00:09:08 -0500

    feat: implement notification service consumer for rabbitmq events

--- apps/notifications-service/src/app.controller.ts
+++ apps/notifications-service/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  
+  // Escuchamos el evento exacto que enviamos desde Auth
+  @EventPattern('user_created')
+  handleUserCreated(@Payload() data: any) {
+    console.log('------------------------------------------------');
+    console.log('🔔 ¡NUEVA NOTIFICACIÓN RECIBIDA DE RABBITMQ!');
+    console.log(`📧 Simulando envío de correo a: ${data.email}`);
+    console.log(`📝 Mensaje: ${data.message}`);
+    console.log(`📅 Fecha: ${data.date}`);
+    console.log('------------------------------------------------');
   }
 }
--- apps/notifications-service/src/main.ts
+++ apps/notifications-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // En lugar de HTTP, usamos Microservice con RabbitMQ
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://admin:adminpassword@localhost:5672'], // Conexión a tu RabbitMQ local
+      queue: 'notifications_queue', // La misma cola donde Auth dejó el mensaje
+      queueOptions: {
+        durable: false
+      },
+    },
+  });
+
+  await app.listen();
+  console.log('📧 Notifications Service está escuchando eventos de RabbitMQ...');
 }
 bootstrap();
