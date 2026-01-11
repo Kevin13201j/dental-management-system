84797e259393a4669ee35da549200d3c65fad8ee
Kevin Amaguaña <71292836+Kevin13201j@users.noreply.github.com>
2026-01-10 22:22:35 -0500

    feat: add swagger documentation to auth-service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configuración de Swagger (Documentación - Req #21)
+  const config = new DocumentBuilder()
+    .setTitle('Dental System Auth API')
+    .setDescription('Microservicio de Autenticación y Usuarios')
+    .setVersion('1.0')
+    .addTag('Auth')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); // La docs estará en /api/docs
+
+  await app.listen(3000);
 }
 bootstrap();
