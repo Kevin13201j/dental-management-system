7118a599a37792cc2b89069ab49edd72435ee71c
Kevin Amaguaña <71292836+Kevin13201j@users.noreply.github.com>
2026-01-23 17:39:18 -0500

    docs: finalize terraform infrastructure with global routing rules

--- infrastructure/QA/main.tf
+++ infrastructure/QA/main.tf
@@ -337,3 +337,20 @@ resource "aws_lb_listener_rule" "appointments_rule" {
     }
   }
 }
+
+# --- C. REGLA POR DEFECTO PARA EL GATEWAY (Maneja el resto del tráfico) ---
+resource "aws_lb_listener_rule" "default_gateway_rule" {
+  listener_arn = aws_lb_listener.front_end.arn
+  priority     = 100 
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"] 
+    }
+  }
+}
